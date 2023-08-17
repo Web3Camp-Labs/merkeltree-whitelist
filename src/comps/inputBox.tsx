371feef1ac30ad6b7f2f7d3934aa1ae6eeba8f24
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-18 00:33:30 +0800

    add line muber

--- src/comps/inputBox.tsx
+++ src/comps/inputBox.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import Textarea from "@mui/joy/Textarea";
 import Button from "@mui/joy/Button";
 import { useCSVReader } from "react-papaparse";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ResultBox from "./resultBox";
 
 export default function InputBox() {
@@ -10,6 +10,7 @@ export default function InputBox() {
   const [inputValue, setInputValue] = useState<string>("");
   const [result, setResult] = useState<any>("");
   const [rootHash, setRootHash] = useState("");
+  const [lines, setLines] = useState<string[]>([]);
 
   const handleCSVfile = (data: string[][]) => {
     console.log(data);
@@ -25,6 +26,18 @@ export default function InputBox() {
     setRootHash("");
   };
 
+  useEffect(() => {
+    const formatNumber = (n: number) => {
+      if (n < 10) {
+        return `0${n}`;
+      } else {
+        return `${n}`;
+      }
+    };
+    const num = inputValue.split("\n");
+    setLines(num.map((_, i) => formatNumber(i + 1)));
+  }, [inputValue]);
+
   return (
     <InputBoxStyle>
       {result ? (
@@ -35,17 +48,27 @@ export default function InputBox() {
         />
       ) : (
         <>
-          <Textarea
-            variant="solid"
-            placeholder="Type anything…"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            minRows={15}
-            maxRows={15}
-            sx={{
-              "--Textarea-focusedHighlight": "unset",
-            }}
-          />
+          <MainInputBox>
+            <ul className="line">
+              {lines.map((i) => (
+                <li key={i}>{i}</li>
+              ))}
+            </ul>
+            <div className="input">
+              <Textarea
+                variant="solid"
+                placeholder="Type anything…"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                minRows={15}
+                sx={{
+                  "--Textarea-focusedHighlight": "unset",
+                }}
+                style={{ background: "unset" }}
+              />
+            </div>
+          </MainInputBox>
+
           <ChooseFileBox>
             <CSVReader
               onUploadAccepted={(results: { data: string[][] }) => {
@@ -83,6 +106,29 @@ const ChooseFileBox = styled.div`
   justify-content: space-between;
   gap: 10px;
   .btn-generate {
+    flex: 1;
+  }
+`;
+
+const MainInputBox = styled.div`
+  background-color: #636b74;
+  border-radius: 6px;
+  display: flex;
+  height: 376px;
+  overflow-y: auto;
+  .line {
+    margin: 0;
+    min-width: 50px;
+    border-right: 1px solid rgba(0, 0, 0, 0.3);
+    padding-block: 8px;
+    list-style: none;
+    padding-left: 0;
+    color: rgba(0, 0, 0, 0.5);
+    li {
+      line-height: 1.5;
+    }
+  }
+  .input {
     flex: 1;
   }
 `;
